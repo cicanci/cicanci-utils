a765eab5b47c030ee1e68bddbd13831a54ec67a6
Bruno Cicanci <brunocicanci@gmail.com>
2020-04-04 17:51:07 +0100

    Updated samples structure

---
+++ Runtime/MonoProxy.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace Cicanci.Utils
+{
+    public class MonoProxy : MonoBehaviour
+    {
+        private static MonoProxy _instance;
+        public static MonoProxy Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    var singletonObject = new GameObject("_MonoProxy");
+                    _instance = singletonObject.AddComponent<MonoProxy>();
+                    DontDestroyOnLoad(singletonObject);
+                }
+                return _instance;
+            }
+        }
+
+        public delegate void AwakeEventHandler();
+        public delegate void StartEventHandler();
+        public delegate void DestroyEventHandler();
+        public delegate void UpdateEventHandler();
+
+        public event AwakeEventHandler AwakeEvent;
+        public event StartEventHandler StartEvent;
+        public event DestroyEventHandler DestroyEvent;
+        public event UpdateEventHandler UpdateEvent;
+
+        private void Awake()
+        {
+            AwakeEvent?.Invoke();
+        }
+
+        private void Start()
+        {
+            StartEvent?.Invoke();
+        }
+
+        private void OnDestroy()
+        {
+            DestroyEvent?.Invoke();
+        }
+
+        private void Update()
+        {
+            UpdateEvent?.Invoke();
+        }
+    }
+}
---
+++ Samples/Scripts/MonoProxyTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cicanci.Utils.Samples
+{
+    public class MonoProxyTest : MonoBehaviour
+    {
+
+    }
+}
