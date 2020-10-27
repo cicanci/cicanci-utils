179924eae40e0e883bac4637b24d7f9217a48d7c
Bruno Cicanci <brunocicanci@gmail.com>
2020-10-27 00:38:27 +0000

    Renamed the symbol

--- Runtime/Debug/LogService.cs
+++ Runtime/Debug/LogService.cs
@@ -4,19 +4,17 @@ namespace Cicanci.Utils
 {
     public class LogService : BaseService
     {
-        [Conditional("ENABLE_DEBUG_LOG")]
+        [Conditional("DEBUG_LOG")]
         public void Log(string message)
         {
             UnityEngine.Debug.Log($"[{GetType()}] {message}");
         }
 
-        [Conditional("ENABLE_DEBUG_LOG")]
         public void LogWarn(string message)
         {
             UnityEngine.Debug.LogWarning($"[{GetType()}] {message}");
         }
 
-        [Conditional("ENABLE_DEBUG_LOG")]
         public void LogError(string message)
         {
             UnityEngine.Debug.LogError($"[{GetType()}] {message}");
